a4296fbee57aae40858208ed1ba79fab91b6fa6f
Tristan Boulanger <contact@tristanboulanger.fr>
2018-06-09 20:44:45 +0200

    [Opti] Convert tabs in spaces

--- src/app.js
+++ src/app.js
@@ -1,38 +1,38 @@
 const app = angular.module('App', []);
 
 app.controller('todoController', function($scope){
-	$scope.input = "";
-	$scope.title = "Ma todolist en AngularJs";
-	$scope.todos = [
-		{
-			"text": "Créer une todo",
-			"done": false
-		},
-		{
-			"text": "Tester mon application",
-			"done": false
-		}
-	];
+  $scope.input = "";
+  $scope.title = "Ma todolist en AngularJs";
+  $scope.todos = [
+    {
+      "text": "Créer une todo",
+      "done": false
+    },
+    {
+      "text": "Tester mon application",
+      "done": false
+    }
+  ];
 
-	$scope.addTodo = () => {
-		if($scope.input.length > 0) {
-			$scope.todos.push({
-				"text": $scope.input,
-				"done": false
-			});
-			$scope.input = "";
-		} else {
-			alert('Veuillez saisir au moins 1 caractère.');
-		}
-	}
-	$scope.editTodo = key => {
-		$scope.todos[key].text = prompt("Changer la todo :", $scope.todos[key].text);
-	}
-	$scope.removeTodo = key => {
-		if(confirm("Êtes-vous sûr de vouloir suprimer cette todo ?")){
-			$scope.todos.splice(key, 1);
-		} else {
-			return false;
-		}
-	}
+  $scope.addTodo = () => {
+    if($scope.input.length > 0) {
+      $scope.todos.push({
+        "text": $scope.input,
+        "done": false
+      });
+      $scope.input = "";
+    } else {
+      alert('Veuillez saisir au moins 1 caractère.');
+    }
+  }
+  $scope.editTodo = key => {
+    $scope.todos[key].text = prompt("Changer la todo :", $scope.todos[key].text);
+  }
+  $scope.removeTodo = key => {
+    if(confirm("Êtes-vous sûr de vouloir suprimer cette todo ?")){
+      $scope.todos.splice(key, 1);
+    } else {
+      return false;
+    }
+  }
 });
